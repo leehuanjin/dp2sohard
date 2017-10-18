afa4221497323254c29e7f5ca17e34adfd5356ba
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2017-10-18 14:01:46 +0800

    sell on deallo button
    only appears with logged in user

--- Deallo Craft House/index.php
+++ Deallo Craft House/index.php
@@ -65,7 +65,10 @@
                         <div class="col-sm-8">
                             <div class="shop-menu pull-right">
                                 <ul class="nav navbar-nav">
+                                
+                                    <?php  if (isset($_SESSION['username'])) : ?>
                                     <li><a href="sell.html"><i class="fa fa-money"></i> SELL On Deallo</a></li>
+                                    <?php endif ?>
                                     <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
                                     <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
                                     <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
