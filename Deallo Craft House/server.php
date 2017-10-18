e11a33956dfc9629f152af3a75252a3b8787ab91
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2017-10-19 02:03:48 +0800

    product deletion

--- Deallo Craft House/server.php
+++ Deallo Craft House/server.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start ();
 $username = "";
@@ -44,71 +45,95 @@ if(isset($_POST['register'])){
 }
 
 
-    // LOGIN USER
-    if (isset($_POST['login_user'])) {
-        $username = mysqli_real_escape_string($db, $_POST['username1']);
-        $password = mysqli_real_escape_string($db, $_POST['password2']);
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username1']);
+    $password = mysqli_real_escape_string($db, $_POST['password2']);
 
-        if (empty($username)) {
-            array_push($errors, "Username is required");
-        }
-        if (empty($password)) {
-            array_push($errors, "Password is required");
-        }
-
-        if (count($errors) == 0) {
-            $password = md5($password);
-            $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-            $results = mysqli_query($db, $query);
-
-            if (mysqli_num_rows($results) == 1) {
-                $_SESSION['username'] = $username;
-                $_SESSION['success'] = "You are now logged in";
-                header('location: index.php');
-            }else {
-                array_push($errors, "Wrong username/password combination");
-                
-            }
-        }
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
     }
 
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
 
-    //logout
-    if (isset($_GET['logout'])) {
-        session_destroy();
-        unset($_SESSION['username']);
-        header("location: index.php");
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+
+        }
     }
+}
 
+
+//logout
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: index.php");
+}
+
+
+//product upload sesseion!!!!!
+
+
+//if upload button is pressed
+if (isset($_POST['upload'])) {
+
+    $msg = "";
+
+    $target = "images/".basename($_FILES['image']['name']);
+
+
+    $image = $_FILES['image']['name'];
+    $image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+
+    $sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+    mysqli_query($db, $sql);
+
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+        $msg = "Image uploaded successfully";
+    }else{
+        $msg = "Failed to upload image";
+    }
+}
+
+$result = mysqli_query($db, "SELECT * FROM images");
+
+//delete
+if(isset($_POST['delete'])){
+
+
+   $id =  $_SESSION['Fid'];
     
-    //product upload sesseion!!!!!
-   
-
-    //if upload button is pressed
-    if (isset($_POST['upload'])) {
-        
-	    $msg = "";
-        
-		$target = "images/".basename($_FILES['image']['name']);
+    
+    $select = mysqli_fetch_assoc(mysqli_query($db,"SELECT image FROM images WHERE id='$id'")); //Fetch the file which is associated with this account
 
 
-		$image = $_FILES['image']['name'];
-		$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
 
+    unlink("images/".$select['image']); //Deleting the file
 
-		$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
-		mysqli_query($db, $sql);
+    $query=mysqli_query($db,"DELETE FROM images WHERE id='$id'");
+    if($query){
+        header("Location: shop.php");
 
-		if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-			$msg = "Image uploaded successfully";
-		}else{
-			$msg = "Failed to upload image";
-		}
-	}
+    }else{
+        echo "Unable to proccess your request";
+    }
+    $_SESSION['msg']="Your Account Has  Been Deleted";
+    unset($_SESSION['usersession']);
 
-	$result = mysqli_query($db, "SELECT * FROM images");
-
-
+    echo "Unable to proccess your request";
+}
 
 
 
