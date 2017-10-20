5dabb747bc3d9e5ffe8693c6fce3d4ea2dd3cb76
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2017-10-21 01:14:25 +0800

    Add files via upload

--- Deallo Craft House/index.php
+++ Deallo Craft House/index.php
@@ -74,9 +74,15 @@
                                     <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
                                     <li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
                                     <?php 
-                                    if (isset($_SESSION['username'])) : ?>
-                                    <li><a href="login.php"><i class="fa fa-lock"></i> Logout</a></li>
-                                    <?php endif ?>
+                                    if (!isset($_SESSION['username'])){
+                                    echo "<li><a href='login.php'><i class='fa fa-lock'></i> Login</a></li>";
+                                    }
+                                    else 
+                                    {
+                                        echo "<li><a href='index.php?logout='1''><i class='fa fa-lock'></i> Logout</a></li>";
+                                    }
+                                    
+                                    ?>
 
                                 </ul>
                             </div>
@@ -320,33 +326,12 @@
                                 </div>
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
-                                        <h4 class="panel-title"><a href="#">Shoes</a></h4>
+                                        <h4 class="panel-title"><a href="shop.php">Shoes</a></h4>
                                     </div>
                                 </div>
                             </div><!--/category-products-->	
-                            <div class="brands_products"><!--brands_products-->
-                                <h2>Brands</h2>
-                                <div class="brands-name">
-                                    <ul class="nav nav-pills nav-stacked">
-                                        <li><a href="#"> <span class="pull-right">(50)</span>Acne</a></li>
-                                        <li><a href="#"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
-                                        <li><a href="#"> <span class="pull-right">(27)</span>Albiro</a></li>
-                                        <li><a href="#"> <span class="pull-right">(32)</span>Ronhill</a></li>
-                                        <li><a href="#"> <span class="pull-right">(5)</span>Oddmolly</a></li>
-                                        <li><a href="#"> <span class="pull-right">(9)</span>Boudestijn</a></li>
-                                        <li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
-                                    </ul>
-                                </div>
-                            </div><!--/brands_products-->
-
-                            <div class="price-range"><!--price-range-->
-                                <h2>Price Range</h2>
-                                <div class="well text-center">
-                                    <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
-                                    <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
-                                </div>
-                            </div><!--/price-range-->
-
+             
+                   
                             <div class="shipping text-center"><!--shipping-->
                                 <img src="images/home/shipping.jpg" alt="" />
                             </div><!--/shipping-->
@@ -356,7 +341,7 @@
 
                     <div class="col-sm-9 padding-right">
                         <div class="features_items"><!--features_items-->
-                            <h2 class="title text-center">Featured Items</h2>
+                            <h2 class="title text-center">Features Items</h2>
                             <div class="col-sm-4">
                                 <div class="product-image-wrapper">
                                     <div class="single-products">
