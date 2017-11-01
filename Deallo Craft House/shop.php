94a4171aba9751ca9a451536feee3aa36b7e9a51
samueltiong1996 <samuel_tiong@hotmail.com>
2017-11-02 00:21:06 +0800

    Update shop.php

--- Deallo Craft House/shop.php
+++ Deallo Craft House/shop.php
@@ -182,27 +182,11 @@
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordian" href="#mens">
+                                            <a href="#mens">
                                                 <span class="badge pull-right"><i class="fa fa-plus"></i></span>
                                                 Mens
                                             </a>
                                         </h4>
-                                    </div>
-                                    <div id="mens" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <ul>
-                                                <li><a href="">Fendi</a></li>
-                                                <li><a href="">Guess</a></li>
-                                                <li><a href="">Valentino</a></li>
-                                                <li><a href="">Dior</a></li>
-                                                <li><a href="">Versace</a></li>
-                                                <li><a href="">Armani</a></li>
-                                                <li><a href="">Prada</a></li>
-                                                <li><a href="">Dolce and Gabbana</a></li>
-                                                <li><a href="">Chanel</a></li>
-                                                <li><a href="">Gucci</a></li>
-                                            </ul>
-                                        </div>
                                     </div>
                                 </div>
 
